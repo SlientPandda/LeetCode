196f86cadaffb784abe2457e5fa3f89e96c21697
wuhao <2573086878@qq.com>
2022-09-30 11:09:03 +0800

    提交

--- src/算法/codetop/LeetCode474.java
+++ src/算法/codetop/LeetCode474.java
@@ -19,35 +19,26 @@ public class LeetCode474 {
      * @param n 子集中1的最多个数
      * @return
      */
-    public static int findMaxForm(String[] strs, int m, int n) {
+    public int findMaxForm(String[] strs, int m, int n) {
+        //时间复杂度 O(strs.length * m *n) 空间复杂度 O(m * n)
         //相当于两种种类限制条件的背包
         //dp[i][j]表示i个0和j个1时的最大子集
         int[][] dp = new int[m + 1][n + 1];
-        int oneNum, zeroNum;
         for (String str : strs) {
-            oneNum = 0;
-            zeroNum = 0;
-            for (char ch : str.toCharArray()) {
-                if (ch == '0') {
+            int zeroNum = 0, oneNum = 0;
+            for (char c : str.toCharArray()) {
+                if (c == '0') {
                     zeroNum++;
                 } else {
                     oneNum++;
                 }
             }
-            //倒序遍历
             for (int i = m; i >= zeroNum; i--) {
                 for (int j = n; j >= oneNum; j--) {
                     dp[i][j] = Math.max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
                 }
             }
-            System.out.println(Arrays.toString(dp));
         }
         return dp[m][n];
     }
-
-    public static void main(String[] args){
-        System.out.println(findMaxForm(new String[]{"10","0001","111001","1","0"}, 3 ,3));
-    }
-
-
 }
