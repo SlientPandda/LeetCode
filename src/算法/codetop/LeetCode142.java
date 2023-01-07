71e447d40ea226fb19f09b7d3d6271ed87962ec7
wuhao <2573086878@qq.com>
2023-01-07 11:59:27 +0800

    提交leetcode141、142

--- src/算法/codetop/LeetCode142.java
+++ src/算法/codetop/LeetCode142.java
@@ -15,6 +15,18 @@ public class LeetCode142 {
     public ListNode detectCycle(ListNode head) {
 //        快慢指针法判断是否有环
         ListNode fast = head, slow = head;
-
+        while (fast != null && slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode tmp = fast;
+                while (tmp != head) {
+                    tmp = tmp.next;
+                    head = head.next;
+                }
+                return tmp;
+            }
+        }
+        return null;
     }
 }
