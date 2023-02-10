c374388687ad7355448e39304741626739ec2f85
wuhao <2573086878@qq.com>
2023-02-10 16:09:10 +0800

    提交leetcode104等

---
+++ src/算法/codetop/LeetCode226.java
@@ -0,0 +1,29 @@
+package 算法.codetop;/**
+ *
+ */
+
+import 算法.DataStructure.TreeNode;
+
+/**
+ *@ClassName LeetCode226
+ *@Description TODO
+ *@Author wuhao51
+ *@Date 2023/2/10 9:47
+ *@Version 1.0
+ **/
+public class LeetCode226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        recur(root);
+        return root;
+    }
+
+    private void recur(TreeNode root) {
+        if (root == null) return;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        recur(root.left);
+        recur(root.right);
+    }
+}
