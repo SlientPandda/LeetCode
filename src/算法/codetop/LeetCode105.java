cb85bccfe8be6b47e5cf84f0dcd8929acd1d1403
wuhao <2573086878@qq.com>
2023-03-07 15:38:40 +0800

    提交leetcode105等

---
+++ src/算法/codetop/LeetCode105.java
@@ -0,0 +1,14 @@
+package 算法.codetop;/**
+ *
+ */
+
+/**
+ *@ClassName LeetCode105
+ *@Description TODO
+ *@Author wuhao51
+ *@Date 2023/3/3 15:45
+ *@Version 1.0
+ **/
+public class LeetCode105 {
+
+}
---
+++ src/算法/codetop/LeetCode106.java
@@ -0,0 +1,32 @@
+package 算法.codetop;/**
+ *
+ */
+
+import 算法.DataStructure.TreeNode;
+
+/**
+ *@ClassName LeetCode106
+ *@Description TODO
+ *@Author wuhao51
+ *@Date 2023/3/3 10:06
+ *@Version 1.0
+ **/
+public class LeetCode106 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return recur(inorder, postorder, 0, inorder.length - 1, postorder.length - 1);
+    }
+
+    //n代表postorder当前值,i,j代表inorder下标范围
+    private TreeNode recur(int[] inorder, int[] postorder, int i, int j, int n) {
+        if (i > j || n < 0) return null;
+        int inindex = 0;
+        for (int k = i; k <= j; k++) {
+            if (inorder[k] == postorder[n]) inindex = k;
+        }
+        TreeNode root = new TreeNode(postorder[n]);
+        root.right = recur(inorder, postorder, inindex + 1, j, n - 1);
+        root.left = recur(inorder, postorder, i, inindex - 1, n - (j - inindex) - 1);
+        return root;
+    }
+
+}
