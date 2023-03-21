be97e02db2da2da3222c43e124a1d536b3c7c686
wuhao <2573086878@qq.com>
2023-03-21 14:09:05 +0800

    提交leetcode450等

--- src/算法/codetop/LeetCode450.java
+++ src/算法/codetop/LeetCode450.java
@@ -16,11 +16,14 @@ public class LeetCode450 {
         return recur(root, key);
     }
 
+    //返回删除目标节点后的树根节点
     private TreeNode recur(TreeNode root, int key) {
         if(root == null) return null;
         if(root.val == key){
+            //如果左或右为空，则补其另一节点
             if(root.left == null) return root.right;
             if(root.right == null) return root.left;
+            //如果左右不为空，则找到当前节点的左子树的最右侧节点，然后把右子树作为该节点的右子树（反之把左子树作为右子树的最左侧节点的左子树也是一样的）
             TreeNode node = root.left;
             while (node.right != null){
                 node = node.right;
