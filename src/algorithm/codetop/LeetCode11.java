ec69b5c3fc194fad60f92a08977c495a2cdf0e78
wuhao <2573086878@qq.com>
2023-08-29 11:27:17 +0800

    提交leetcode11

---
+++ src/algorithm/codetop/LeetCode11.java
@@ -0,0 +1,27 @@
+package algorithm.codetop;/**
+ *
+ */
+
+/**
+ *@ClassName LeetCode11
+ *@Description TODO
+ *@Author wuhao51
+ *@Date 2023/8/29 10:15
+ *@Version 1.0
+ **/
+public class LeetCode11 {
+    //双指针法，时间复杂度O(N)，空间复杂度O(1)
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int res = Integer.MIN_VALUE;
+        while (i < j) {
+            res = Math.max((j - i) * Math.min(height[i], height[j]), res);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
