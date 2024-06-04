253a182a8d6517b58f5f0587d9b0fb9234b3c927
SlientPandda <henrywukk@gmail.com>
2024-06-04 13:31:16 +0800

    提交leetcode283

--- src/algorithm/codetop/LeetCode283.java
+++ src/algorithm/codetop/LeetCode283.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
  *@Version 1.0
  **/
 public class LeetCode283 {
-    public static void moveZeroes(int[] nums) {
+    public void moveZeroes(int[] nums) {
         // 使用双指针，左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
         // 右指针不断向右移动，每次右指针指向非零数，则将左右指针对应的数交换，同时左指针右移，
         // 注意到以下性质：1.左指针左边均为非零数;2.右指针左边直到左指针处均为零。
@@ -29,15 +29,9 @@ public class LeetCode283 {
         }
     }
 
-    private static void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{0, 0, 1};
-        moveZeroes(nums);
-        System.out.println(Arrays.toString(nums));
     }
 }
