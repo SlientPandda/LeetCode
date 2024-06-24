11eda70e821a7cd2e60f4abdc6397752a0c1d667
SlientPandda <henrywukk@gmail.com>
2024-06-24 14:51:59 +0800

    提交leetcode103

---
+++ src/algorithm/binarytree/LeetCode103.java
@@ -0,0 +1,50 @@
+package algorithm.binarytree;/**
+ *
+ */
+
+import algorithm.datastructure.TreeNode;
+
+import java.util.*;
+
+/**
+ *@ClassName LeetCode103
+ *@Description TODO
+ *@Author wuhao51
+ *@Date 2024/6/20 23:19
+ *@Version 1.0
+ **/
+public class LeetCode103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> nodeQueue = new ArrayDeque<TreeNode>();
+        nodeQueue.offer(root);
+        boolean isOrderLeft = true;
+
+        while (!nodeQueue.isEmpty()) {
+            Deque<Integer> levelList = new LinkedList<Integer>();
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode curNode = nodeQueue.poll();
+                if (isOrderLeft) {
+                    levelList.offerLast(curNode.val);
+                } else {
+                    levelList.offerFirst(curNode.val);
+                }
+                if (curNode.left != null) {
+                    nodeQueue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    nodeQueue.offer(curNode.right);
+                }
+            }
+            ans.add(new LinkedList<Integer>(levelList));
+            isOrderLeft = !isOrderLeft;
+        }
+
+        return ans;
+    }
+}
