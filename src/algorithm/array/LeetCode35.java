4228ab886b18db9115a3ac4f3e133ec85259a381
wuhao <henrywukk@gmail.com>
2024-08-01 22:41:18 +0800

    提交leetcode35

--- src/algorithm/array/LeetCode35.java
+++ src/algorithm/array/LeetCode35.java
@@ -11,12 +11,11 @@ package algorithm.array;/**
  **/
 public class LeetCode35 {
     public int searchInsert(int[] nums, int target) {
-        // 1 3 4 6
         if (nums.length == 0) return 0;
         // 左右区间边界
         int i = 0, j = nums.length - 1;
         int mid = 0;
-        while (i < j) {
+        while (i <= j) {
             mid = i + (j - i) / 2;
             if (nums[mid] == target) return mid;
             // 如果查询值比目标值大，说明目标值在中点左边区间内
@@ -24,6 +23,7 @@ public class LeetCode35 {
             // 反之说明在中点右边区间内
             else i = mid + 1;
         }
-        return mid + 1;
+        // 此时 i 都大于 j，说明 target在（j， i）区间内 直接取i即为待插入位置
+        return i;
     }
 }
